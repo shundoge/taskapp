97af28ba6c84e2ed1f5fe6d41b2c5b24d79f38a8
TANHotmail <ttt93764672976394112233@hotmail.com>
2016-03-21 18:28:42 +0900

    削除OK

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -7,20 +7,27 @@
 //
 
 import UIKit
+import RealmSwift
 
 class InputViewController: UIViewController {
-
+    
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
+    
+    let realm = try! Realm()
+    var task:Task!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    // 各セルを選択した時に実行されるメソッド
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        performSegueWithIdentifier("cellSegue",sender: nil) // ←追加する
+        
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:"dismissKeyboard")
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
     }
     
     override func didReceiveMemoryWarning() {
@@ -28,6 +35,22 @@ class InputViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillDisappear(animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date
+            self.realm.add(self.task, update: true)
+        }
+        
+        super.viewWillDisappear(animated)
+    }
+    
+    func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
+    }
+
 
     /*
     // MARK: - Navigation
